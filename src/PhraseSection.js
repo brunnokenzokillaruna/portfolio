7d1db3c09326f8ae31f28cb5973ef2f1f9fed524
Brunno Kenzo <brunnokenzokillaruna@gmail.com>
2024-02-29 08:23:37 -0500

    teste commit

--- src/PhraseSection.js
+++ src/PhraseSection.js
@@ -5,7 +5,7 @@ function PhraseSection() {
   return (
     <section className="PhraseSection">
       <p>
-        Picture a place where tech and creativity come together, making your online presence better. I turn new ideas into great digital projects, focusing on cool designs and strong tech to push your brand into the top of the digital world.
+        Brunno. Picture a place where tech and creativity come together, making your online presence better. I turn new ideas into great digital projects, focusing on cool designs and strong tech to push your brand into the top of the digital world.
       </p>
     </section>
   );
