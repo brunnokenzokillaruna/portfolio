395b578f3183f40530fdf7182cba26c66819edfa
Brunno Kenzo <brunnokenzokillaruna@gmail.com>
2024-02-29 11:07:18 -0500

    few changes

--- src/ContactSection.js
+++ src/ContactSection.js
@@ -5,12 +5,8 @@ import githubIcon from './images/github.svg';
 import linkedinIcon from './images/linkedin.svg';
 
 function ContactSection() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("Thank you for reaching out! Your message has been sent successfully. I will get back to you as soon as possible.");
-  };
-
   const [currentTime, setCurrentTime] = useState('');
+
   const updateTime = () => {
     const now = new Date();
     const timeString = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
@@ -26,17 +22,35 @@ function ContactSection() {
   const ref = useRef(null);
   const isVisible = useIntersectionObserver(ref, { threshold: 0.1 });
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const response = await fetch("https://formspree.io/f/xeqykvev", {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      },
+    });
+
+    if (response.ok) {
+      alert("Thank you for reaching out! Your message has been sent successfully. I will get back to you as soon as possible.");
+    } else {
+      alert("Oops! Something went wrong. Please try again.");
+    }
+  };
+
   return (
     <section className="ContactSection" id="contact" ref={ref}>
       <h2 className={`ContactTitle ${isVisible ? 'visible' : ''}`}>Contact Me</h2>
       <div className="ContactContent">
         <div className="ContactForm">
-          <p>Have an awesome idea? Let's bring it to life together.</p>
-          <p>I am open to freelance opportunities or internships in startups, agencies, and companies.</p>
+          <p>Got an incredible idea? Let's collaborate and turn it into reality.</p>
+          <p>I am actively seeking opportunities and internships with startups,agencies, and companies.</p>
           <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Your Name" required />
-            <input type="email" placeholder="Your Email" required />
-            <textarea placeholder="Your Message" required />
+            <input type="text" name="name" placeholder="Your Name" required />
+            <input type="email" name="email" placeholder="Your Email" required />
+            <textarea name="message" placeholder="Your Message" required></textarea>
             <button type="submit">Send Message</button>
           </form>
         </div>
@@ -60,7 +74,7 @@ function ContactSection() {
           <div className="Location">
             <h3>Location</h3>
             <p>Montreal/Canada</p>
-            <p>Current Time: {currentTime}</p>
+            <p className="timeValue">{currentTime}</p>
           </div>
         </div>
       </div>
