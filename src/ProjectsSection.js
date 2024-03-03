23d9028261ce56c7297ae4e0dc5c9e8e91085ef9
Brunno Kenzo <brunnokenzokillaruna@gmail.com>
2024-03-03 15:15:13 -0500

    Final Commit

--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -1,9 +1,12 @@
 import React, { useRef } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import useIntersectionObserver from './useIntersectionObserver';
 import './ProjectsSection.css';
 import projectImage1 from './images/infostream.png';
 import projectImage2 from './images/grandeur.png';
 import projectImage3 from './images/wealthwise.png';
+import projectImage4 from './images/TriviaGame.png';
 
 const projects = [
   {
@@ -27,33 +30,52 @@ const projects = [
     technologies: "Windows Forms, C#",
     github: "https://github.com/brunnokenzokillaruna/wealthwise/"
   },
+  {
+    name: "Trivia Game",
+    image: projectImage4,
+    year: "2023",
+    technologies: "Python",
+    github: "https://github.com/brunnokenzokillaruna/TriviaQuizGame"
+  },
 ];
+
+function chunkArray(array, chunkSize) {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+  return chunks;
+}
 
 function ProjectsSection() {
   const ref = useRef(null);
   const isVisible = useIntersectionObserver(ref, { threshold: 0.1 });
+  const projectChunks = chunkArray(projects, 2);
 
   return (
     <section className="ProjectsSection" id="projects" ref={ref}>
       <h2 className={`ProjectsTitle ${isVisible ? 'visible' : ''}`}>My Projects</h2>
-      <div className="ProjectsContent">
-        {projects.map((project, index) => (
-          <div key={index} className="ProjectItem">
-            <div className="ProjectImageWrapper">
-              <img src={project.image} alt="Project" className="ProjectImage" />
-              <div className="ProjectOverlay">{project.name}</div>
-            </div>
-            <div className="ProjectLabels">
-              <span>{project.year}</span>
-              <span>{project.technologies}</span>
-              <a href={project.github}>GitHub</a>
-            </div>
+      <Carousel className="ProjectsCarousel" showArrows={true} autoPlay={false} infiniteLoop={true} centerMode={false}>
+        {projectChunks.map((chunk, index) => (
+          <div key={index} className="ProjectPair">
+            {chunk.map((project, projectIndex) => (
+              <div key={projectIndex} className="ProjectItem">
+                <div className="ProjectImageWrapper">
+                  <img src={project.image} alt={project.name} className="ProjectImage" />
+                  <div className="ProjectOverlay">{project.name}</div>
+                </div>
+                <div className="ProjectLabels">
+                  <span>{project.year}</span>
+                  <span>{project.technologies}</span>
+                  <a href={project.github}>GitHub</a>
+                </div>
+              </div>
+            ))}
           </div>
         ))}
-      </div>
+      </Carousel>
     </section>
   );
 }
-
 
 export default ProjectsSection;
